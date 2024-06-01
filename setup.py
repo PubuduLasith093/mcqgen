3eb86f0fb19d7079ce921f8a1b4c686e20347960
pubudu lasith <pubudu093@gmail.com>
2024-06-01 12:42:12 +1000

    'commit'

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='pubudu lasith',
     author_email='pubudu093@gmail.com',
-    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    install_requires=["langchain_openai","langchain","streamlit","python-dotenv","PyPDF2"],
     packages=find_packages()
 )
